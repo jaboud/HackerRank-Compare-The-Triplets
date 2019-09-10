eecc818ddbcef14b8b629cebce066ef9a20cac9e
jaboud <jaboud94@gmail.com>
2019-09-10 21:13:30 +1000

    Created a list to store the scores and started the default scores for both Alice & Bob at 0.

--- HackerRank-Compare-The-Triplets/Program.cs
+++ HackerRank-Compare-The-Triplets/Program.cs
@@ -7,9 +7,11 @@ namespace HackerRank_Compare_The_Triplets
     {
         static List<int> CompareTriplets(List<int> a, List<int> b)
         {
+            //List of integers to store the scores. Scores set to 0 at default
+            List<int> scores = new List<int>() { 0, 0 };
 
 
-            return null;
+            return scores;
         }
 
             static void Main(string[] args)
