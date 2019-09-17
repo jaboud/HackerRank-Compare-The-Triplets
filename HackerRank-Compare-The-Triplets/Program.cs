018b8841026e2d640081a80749a6a5c7e820703e
jaboud <jaboud94@gmail.com>
2019-09-17 14:54:57 +1000

    Changed from using a list to Int to run the program much faster.

--- HackerRank-Compare-The-Triplets/Program.cs
+++ HackerRank-Compare-The-Triplets/Program.cs
@@ -7,39 +7,42 @@ namespace HackerRank_Compare_The_Triplets
 {
     class Program
     {
-        static List<int> CompareTriplets(List<int> a, List<int> b)
+        static int[] CompareTriplets(int[] a, int[] b)
         {
             //List of integers to store the scores. Scores set to 0 at default
-            List<int> scores = new List<int>() { 0, 0 };
+            int[] scores = new int[] { 0, 0 };
 
             //For loop to see if one element is greater than the other. Program will increment score if this is the case.
-            for (int i = 0; i < a.Count; i++)
+            for (int i = 0; i < a.Length; i++)
             {
-                if (a[i] > b[i]) scores[0]++;
-                else if (a[i] < b[i]) scores[1]++;
+                if (a[i] > b[i])
+                    scores[0]++;
+                else
+                    scores[1]++;
             }
 
             return scores;
         }
 
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            using (TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true))
+            {
+                //For the user to input the first integer element into the console
+                int[] a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToArray();
 
-            //For the user to input the first integer element into the console
-            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+                //For the user to input the second integer element into the console
+                int[] b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToArray();
 
-            //For the user to input the second integer element into the console
-            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+                //Prints out the score comparisons
+                int[] result = CompareTriplets(a, b);
 
-            //Prints out the score comparisons
-            List<int> result = CompareTriplets(a, b);
+                //Prints out the final result between the two elements
+                textWriter.WriteLine(String.Join(" ", result));
 
-            //Prints out the final result between the two elements
-            textWriter.WriteLine(String.Join(" ", result));
-
-            textWriter.Flush();
-            textWriter.Close();
+                textWriter.Flush();
+                textWriter.Close();
+            }
 
             //To prevent the console from automatically closing upon completion of program
             Console.Read();
